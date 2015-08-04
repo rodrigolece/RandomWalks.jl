d7328a8d4c4587d1d70c592c759b95f845d53d6b
Rodrigo Leal Cervantes <rodrigolealc@ciencias.unam.mx>
2015-08-04 11:47:09 -0500

    Agregué pruebas para addEdge\!

--- src/tests_nets.jl
+++ src/tests_nets.jl
@@ -13,4 +13,9 @@ facts("Pruebas del módulo Nets") do
 	# Pruebas de la condición de frontera cíclica
 	@fact 2 in getNeighbours(w,1) --> true
 	@fact num_nodes in getNeighbours(w,1) --> true
+
+	n1 = 1 ; n2 = 5
+	addEdge!(w,n1,n2)
+	@fact n1 in getNeighbours(w,n2) --> true
+	@fact n2 in getNeighbours(w,n1) --> true
 end
