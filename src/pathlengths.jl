081a816c2c891e765764d86e0ecd5344464e5042
Rodrigo Leal Cervantes <rodrigolealc@ciencias.unam.mx>
2015-08-05 10:47:53 -0500

    Hice módulo PathLengths, corregí funciones

--- src/pathlengths.jl
+++ src/pathlengths.jl
@@ -1,61 +1,69 @@
+module PathLengths
+
+using Nets
+
+export pathLengthsFromNode, allPathLengths, avgPathLength, maxPathLength
 
 ## Distancia entre nodos -------------------------- ##
 
-function PathLengthsFromNode(w::SmallWorldNet, n::Int)
-    d = 0 
+function pathLengthsFromNode(w::SmallWorldNet, n::Int)
+    d = 0
     distances = [n => d]
-    currentShell = [n]
+    current_shell = [n]
 
-    while length(currentShell) > 0
-        nextShell = Int[]
-        for m in currentShell
-            for p in GetNeighbours(w, m)
+    while length(current_shell) > 0
+        next_shell = Int[]
+        for m in current_shell
+            for p in getNeighbours(w, m)
                 if !(p in keys(distances))  #si p no está en el diccionario:
-                    push!(nextShell, p)
+                    push!(next_shell, p)
                     distances[p] = d+1
                 end
             end
         end
         d += 1
-        currentShell = nextShell
+        current_shell = next_shell
     end
-    
+
     return distances
 end
 
-function AllPathLengths(w::SmallWorldNet)
-    out = zeros(fld(w.L,w.Z)+1) #La longitud maxima 
+function allPathLengths(w::SmallWorldNet)
+    out = zeros(Int, fld(w.num_nodes, w.num_neighs) + 1) #La longitud maxima
 
-    for n in GetNodes(w)
-        for n2 in values(PathLengthsFromNode(w, n))
+    for n in 1:w.num_nodes
+        for n2 in values(pathLengthsFromNode(w, n))
             out[n2+1] += 1
         end
     end
-    
-    out /= 2 ; out[1]*=2 #Todas las distancias se cuentan dos veces
-    return out           #excepto la distancia de un nodo a él mismo
+
+    out = int(out/2) ; out[1]*=2 #Todas las distancias se cuentan dos veces
+    return out           		#excepto la distancia de un nodo a él mismo
 end
 
-function AvgPathLength(w::SmallWorldNet)
-    distrib = AllPathLengths(w)
+function avgPathLength(w::SmallWorldNet)
+    distrib = allPathLengths(w)
     p = 0.
-    total = 0.
-    
+    total = 0
+
     for i in 1:length(distrib)
-        p += (i-1)*distrib[i]
+        p += (i-1)*distrib[i] #el -1 se debe a que distrib[1] es la distancia 0
         total += distrib[i]
     end
-    
+
     p /= total
 end
 
-function MaxPathLength(w::SmallWorldNet)
-    distrib = AllPathLengths(w)
+function maxPathLength(w::SmallWorldNet)
+    distrib = allPathLengths(w)
     N = length(distrib)
 
+	# La distancia máxima es la mayor entrada distinta de cero
     for i in 1:N
-        if distrib[N-i] != 0.
+        if distrib[N-i] != 0
             return N-i-1 #el -1 se debe a que distrib[1] es la distancia 0
         end
     end
+end
+
 end
