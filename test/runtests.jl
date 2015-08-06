59af1d8231f425d0ae527d1ee18f3b55bf8d0058
Rodrigo Leal Cervantes <rodrigolealc@ciencias.unam.mx>
2015-08-06 10:32:23 -0500

    Agruegué runtests.jl

---
+++ test/runtests.jl
@@ -0,0 +1,18 @@
+using FactCheck
+
+
+# Pruebas de las redes:
+
+include("tests_nets.jl")
+
+
+# Pruebas de lo algoritmos de distancia:
+
+include("tests_paths.jl")
+
+# Pruebas de las funciones de RW:
+
+include("tests_rw.jl")
+
+
+FactCheck.exitstatus()
