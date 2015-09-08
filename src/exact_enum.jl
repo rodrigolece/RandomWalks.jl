e90b7a45324bc655dee76d8153430a979b0bd34e
Rodrigo Leal Cervantes <rodrigolealc@ciencias.unam.mx>
2015-09-08 12:06:32 -0500

    Mejoré eficiencia de las funciones

--- src/exact_enum.jl
+++ src/exact_enum.jl
@@ -4,9 +4,7 @@ deg(w::SmallWorldNet, node::Int) = length(getNeighbours(w,node))
 deg(z::Net2D, site::(Int,Int)) = length(z.neighbours[site])
 
 
-function exactEnum2D(z::Net2D, p_mat::Array{Float64,2})
-    new_p = zeros(p_mat)
-
+function exactEnum2D(z::Net2D, p_mat::Array{Float64,2}, old_p_mat::Array{Float64,2})
     # Calculamos la proba de encuentro
     p = sum(diag(p_mat))
 
@@ -18,13 +16,19 @@ function exactEnum2D(z::Net2D, p_mat::Array{Float64,2})
 
     # Actualizamos las probabilidades
 
-    for site in keys(z.neighbours)
-        for neigh in z.neighbours[site]
-            new_p[site...] += p_mat[neigh...]/deg(z,neigh)
+    for (site, neighs) in z.neighbours
+        contribution = p_mat[site...]/deg(z,site)
+        p_mat[site...] = 0.
+
+        for neigh in neighs
+            # Estamos suponiendo que old_p_mat siempre es un arreglo de ceros
+            old_p_mat[neigh...] += contribution
         end
+
     end
 
-    p, new_p
+    # Se hace el swap de las matrices
+    p, old_p_mat, p_mat
 end
 
 function firstEncounterEE(z::Net2D, first_node::Int, second_node::Int, num_iters::Int)
@@ -34,12 +38,12 @@ function firstEncounterEE(z::Net2D, first_node::Int, second_node::Int, num_iters
     p_mat = zeros(z.num_nodes,z.num_nodes)
     p_mat[first_node,second_node] = 1.
 
+    old_p_mat = zeros(p_mat)
+
     for iter in 1:num_iters
-        p, p_mat = exactEnum2D(z, p_mat)
+        p, p_mat, old_p_mat = exactEnum2D(z, p_mat, old_p_mat)
         p_encounter[iter] = p
     end
-
-	# Descartamos primeras entradas que son cero ?
 
 	p_encounter, p_mat
 end
