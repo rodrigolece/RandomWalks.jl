65510828392fd7c90ea89af878778b1c1b081334
Rodrigo Leal Cervantes <rodrigolealc@ciencias.unam.mx>
2015-11-06 13:02:32 -0600

    Escribí versión de circleGraph para guardar la imagen

--- src/circlegraph.jl
+++ src/circlegraph.jl
@@ -39,3 +39,28 @@ function circleGraph{T<:ComplexNetwork}(w::T)
 
 	plot(xs, ys, "ro")
 end
+
+function circleGraph{T<:ComplexNetwork}(w::T, name::AbstractString)
+	fig = figure(figsize=(5,5))
+	axes = fig[:gca]()[:axes]
+	axes[:get_xaxis]()[:set_visible](false)
+	axes[:get_yaxis]()[:set_visible](false)
+
+	δ = 0.05
+    xlim(-1-δ, 1+δ)
+    ylim(-1-δ, 1+δ)
+
+    xs, ys = nodeLocations(w)
+
+	# Tal vez esto se puede hacer con un sólo llamado a plot, construyendo una matriz antes
+    for n in 1:w.num_nodes
+        for n2 in getNeighbours(w,n)
+            if n < n2
+                plot([xs[n], xs[n2]], [ys[n],ys[n2]], "k-")
+            end
+        end
+    end
+
+	plot(xs, ys, "ro")
+	savefig(name*".pdf", bbox_inches="tight")
+end
